ba47c9c15df1edf341b5ec2b3bb7f6aca7fcbbcc
vitasika <vitasika1@gmail.com>
2022-04-04 01:17:27 +0300

    проба запроса к серверу

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,25 @@
 // именованный экспорт ф-ции fetchCountries
 export { fetchCountries };
-
-function fetchCountries(name) {
     
+// путь к API - эндпоинт, базовый URL, точка входа в API.
+const BASE_URL = 'https://restcountries.com/v3.1'
+
+const fetchCountries = fetch(BASE_URL).then(response => {
+    console.log(response.json());
+});
+
+
+
+
+
+/*    
+function fetchCountries(name) {
+    console.log('https://restcountries.com/v3.1/')
 }
 
 
 
-/*
+
 нужны только следующие свойства:
 
 name.official - полное имя страны
