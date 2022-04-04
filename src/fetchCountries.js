b43e7834f8938c98589b83c26cdf4f4bca094886
vitasika <vitasika1@gmail.com>
2022-04-04 20:47:15 +0300

    10_0_2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
 // путь к API - эндпоинт, базовый URL, точка входа в API.
 const BASE_URL = 'https://restcountries.com/v3.1/name';
-// заправшиваемые параметры от сервера.
+// заправшиваемые параметры от сервера (выбор нужных значений в capital,flags,languages,name,population запросе).
 const requestParameters = 'fields=name,capital,population,flags,languages';
 
-function fetchCountries(name) {
-    return fetch(`${BASE_URL}/?${searchParams}`)
+function fetchCountries() {
+    return fetch(`${BASE_URL}/?${requestParameters}`)
         .then(response => response.json())
         .catch(error => console.log(error));
 };
@@ -24,7 +24,7 @@ export { fetchCountries };
 // // Ф-ция для составления запросов к серверу API, принимает параметр name (строка - ресурс, к которому мы обращаемся в API), 
 // // возвращает результат вызова метода fetch() - промис, который
 // function fetchCountries(name) {
-//     return fetch(`${BASE_URL}/name/${name}?${searchParams}`) //    передает результат промиса (fulfilled) данные - в параметр (response) метода then(), который:
+//     return fetch(`${BASE_URL}/name/${name}?${searchParams}`) //передает результат промиса (fulfilled) данные - в параметр (response) метода then(), который:
 //     .then(response => {
 //         if (!response.ok) {
 //             throw new Error(response.status); //- если response не оk, принудительно вызывает ошибку (throw new Error(response.status)) - error, т.е. формирует результат rejected;
