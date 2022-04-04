9a8ba175c07beb700f194899838284c4f8795c85
vitasika <vitasika1@gmail.com>
2022-04-04 13:50:59 +0300

    вариант fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,25 @@
-// именованный экспорт ф-ции fetchCountries
-export { fetchCountries };
-    
 // путь к API - эндпоинт, базовый URL, точка входа в API.
 const BASE_URL = 'https://restcountries.com/v3.1'
-
-const fetchCountries = fetch(BASE_URL).then(response => {
-    console.log(response.json());
+// параметры настроек (выборки) запроса
+const searchParams = new URLSearchParams({
+    fields: "capital,flags,languages,name,population",
 });
+
+// Ф-ция для составления запросов к серверу API, принимает параметр name (строка - ресурс, к которому мы обращаемся в API), возвращает результат вызова метода fetch() - промис, который
+//    передает результат промиса (fulfilled) данные - в параметр (response) метода then(), который:
+//             - если response не оk, принудительно вызывает ошибку (throw new Error(response.status)) - error, т.е. формирует результат rejected;
+//             - иначе возвращает полученные данные (из JSON формата) response.json()
+function fetchCountries(name) {
+    return fetch(`${BASE_URL}/name/${name}?${searchParams}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+};
+// именованный экспорт ф-ции fetchCountries
+export { fetchCountries };
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { fetchCountries } from "./fetchCountries.js";
 const DEBOUNCE_DELAY = 300;
 
 
-console.log(fetchCountries);
+// console.log(fetchCountries);
