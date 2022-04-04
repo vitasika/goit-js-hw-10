7a638924eb147e8cfc98bef3f5270dd511ed3ca2
vitasika <86631512+vitasika@users.noreply.github.com>
2022-04-04 17:27:47 +0000

    Deploying to gh-pages from @ vitasika/goit-js-hw-10@464a1f460114294fe1d70c60872bd9127a523f39 🚀

--- src.fcc94eb1.js
+++ src.fcc94eb1.js
@@ -1,8 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"krre":[function(require,module,exports) {
 
 },{}],"SvwL":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.fetchCountries=r;const e="https://restcountries.com/v3.1",t=new URLSearchParams({fields:"capital,flags,languages,name,population"});function r(r){return fetch(`${e}/name/${r}?${t}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;const e="https://restcountries.com/v3.1/name/",t="name,capital,population,flags,languages";function s(s){return fetch(`${e}${s}?fields=${t}`).then(e=>e.json())}var o={fetchCountries:s};exports.default=o;
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./css/styles.css");var s=require("./fetchCountries.js");const e=300;
+"use strict";require("./css/styles.css");var s=require("./fetchCountries.js");const e=300;console.log(s.fetchCountries);
 },{"./css/styles.css":"krre","./fetchCountries.js":"SvwL"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10/src.02cf2be3.js.map
+//# sourceMappingURL=/goit-js-hw-10/src.fcc94eb1.js.map
